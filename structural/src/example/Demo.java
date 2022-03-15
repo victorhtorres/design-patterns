20983bf904e418c6827ffa2cd5d05c56b77662ae
victor.torres <vtorresm86@gmail.com>
2022-03-14 21:10:58 -0500

    Added adapter in structural patterns

---
+++ structural/src/example/Demo.java
@@ -0,0 +1,32 @@
+package example;
+
+import example.adapters.SquarePegAdapter;
+import example.round.RoundHole;
+import example.round.RoundPeg;
+import example.square.SquarePeg;
+
+public class Demo
+{
+    public static void main(String[] args) {
+        // Round fits round, no surprise.
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Won't compile. Incompatible interface
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+}
---
+++ structural/src/example/adapters/SquarePegAdapter.java
@@ -0,0 +1,26 @@
+package example.adapters;
+
+import example.round.RoundPeg;
+import example.square.SquarePeg;
+
+/**
+ * Adapter allows fitting square pegs into round holes.
+ */
+public class SquarePegAdapter extends RoundPeg
+{
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg)
+    {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius()
+    {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
